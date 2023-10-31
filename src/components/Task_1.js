2ab697ca89f290e87683ff8f9867a2a295e79edb
anjali_kolhatkar <anjalikolhatkar1803@gmail.com>
2023-10-31 22:46:25 +0530

    first-v

---
+++ src/components/Task_1.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Task_1 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  keypress = (event) => {
+    this.setState({ count: event.target.value.split(/\s+/).filter(word => word !== '').length });
+  }
+
+  render() {
+    return (
+      <div class = "d-flex flex-column align-items-center justify-content-center">
+        <h6>Anjali Kolhatkar - 21BCE11171</h6>
+        
+        <h1 className='text-center'>A Responsive Paragraph Word Counter Application using React</h1>
+        
+        {/* <h1 className='text-center'>A Responsive Paragraph Word Counter Application using React</h1> */}
+
+        <textarea class="align-items-center justify-content-center" id="textInput" rows='5' cols='50' onChange={this.keypress}></textarea>
+        <p id="wordcount">Word count: {this.state.count}</p>
+      </div>
+    );
+  }
+}
+
+export default Task_1;
